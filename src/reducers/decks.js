878c1d8f749ee50dade38028ded98a3a1a6f8ca7
Erick Massa Sprengel <erick.sprengel@gmail.com>
2019-03-05 03:41:57 -0300

    feat(redux): add dummy decks' reducer

---
+++ src/reducers/decks.js
@@ -0,0 +1,25 @@
+
+const initialState = {
+  x1234: {
+    title: 'Animais',
+  },
+  x2345: {
+    title: 'Heróis',
+  }
+}
+const decks = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_DECK':
+      return {
+        ...state,
+        x12345: {
+          id: x12345,
+          title: action.title,
+        }
+      }
+    default:
+      return state
+  }
+}
+
+export default decks
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+import decks from './decks'
+// import cards from './cards'
+
+const reducers = combineReducers({
+  decks,
+})
+
+export default reducers
