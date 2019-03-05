5f9ed872d6d0bbb2bccf4db47eb9807b9b0703c1
Erick Massa Sprengel <erick.sprengel@gmail.com>
2019-03-05 03:40:57 -0300

    feat(redux): add base middleware: logger and thunk

---
+++ src/middleware/index.js
@@ -0,0 +1,12 @@
+import thunk from 'redux-thunk'
+import logger from './logger'
+import { applyMiddleware, compose } from 'redux'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const middleware = applyMiddleware(
+  thunk,
+  logger,
+)
+
+export default composeEnhancers(middleware)
+// export default compose(middleware)
