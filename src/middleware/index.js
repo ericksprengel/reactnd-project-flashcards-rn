d8e054b37757dcfa40f055b9b8be37aa2a8a5808
Erick Massa Sprengel <erick.sprengel@gmail.com>
2019-03-05 12:40:42 -0300

    feat(redux): integrate remote-redux-devtools

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,12 +1,12 @@
 import thunk from 'redux-thunk'
-import logger from './logger'
 import { applyMiddleware, compose } from 'redux'
+import logger from './logger'
+import { composeWithDevTools } from 'remote-redux-devtools'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = __DEV__ ? composeWithDevTools : compose
 const middleware = applyMiddleware(
   thunk,
   logger,
 )
 
 export default composeEnhancers(middleware)
-// export default compose(middleware)
